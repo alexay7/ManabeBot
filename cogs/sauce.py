0d17d15b2a7d134efb945119ea3ff98f0929d7a9
alexay7 <43906716+alexay7@users.noreply.github.com>
2022-02-10 17:40:42 +0100

    Added sauce prototype

---
+++ cogs/sauce.py
@@ -0,0 +1,53 @@
+"""Cog responsible for sauce."""
+
+import json
+import discord
+import os
+from discord.ext import commands
+from saucenao_api import SauceNao
+from discord import Embed
+
+#############################################################
+# Variables (Temporary)
+with open("cogs/myguild.json") as json_file:
+    data_dict = json.load(json_file)
+    guild_id = data_dict["guild_id"]
+    join_quiz_channel_ids = data_dict["join_quiz_1_id"]
+    admin_id = data_dict["kaigen_user_id"]
+#############################################################
+
+# BOT'S COMMANDS
+
+
+class Sauce(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @ commands.Cog.listener()
+    async def on_ready(self):
+        self.myguild = self.bot.get_guild(guild_id)
+        # await self.private_admin_channel.send("Connected to db successfully")
+
+    @commands.command()
+    async def sauce(self, ctx, url=None):
+        sauce = SauceNao(os.getenv("SAUCE_TOKEN"))
+        if len(ctx.message.attachments) > 0:
+            url = ctx.message.attachments[0].url
+
+        best = sauce.from_url(url)[0]
+
+        if best:
+            embed = Embed(color=0x5842ff)
+            embed.set_thumbnail(url=best.thumbnail)
+            embed.add_field(name="Parecido",
+                            value=best.similarity, inline=False)
+            embed.add_field(name="Nombre", value=best.title, inline=True)
+            embed.add_field(name="Autor", value=best.author, inline=True)
+            embed.add_field(name="Enlace", value=best.urls[0], inline=True)
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send("Nada encontrado")
+
+
+def setup(bot):
+    bot.add_cog(Sauce(bot))
